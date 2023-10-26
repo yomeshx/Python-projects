191ac1d6f9811b6bcf563a89e927a66d6d4b15e4
Yomesh Dissanayaka <yomeshthecoder@gmail.com>
2023-10-26 15:19:48 +0530

    python Hash triangle simple test project

---
+++ Test Projects/Begginer to Advanced/Hash-triangle/hash-triangle.py
@@ -0,0 +1,6 @@
+#This print triangle of '#'s
+t = 11
+c = 9
+for x in range(t):
+    f= x * "#"
+    print(f)
---
+++ test.py
@@ -0,0 +1,6 @@
+#This file is dedicaded to run tests
+t = 11
+c = 9
+for x in range(t):
+    f= x * "#"
+    print(f)
