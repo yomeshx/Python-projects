6c569e51b7b24273e207d6ffe6536d846d4a047b
yomesh-dk <yomeshofficial@gmail.com>
2023-11-15 06:02:23 +0530

    While for

--- Theory testing/For loops.py
+++ Theory testing/For loops.py
@@ -3,12 +3,15 @@ cities = ["Kandy","Colombo","Kurunegala","Galle","Anuradhapura"]
 for city in cities :
     print("Nearest City ,",city)
 
-print("\n\nOver\n")
 
 
-
-
-
+#simple program that return the sum of all the numbers from 1 to 100
+total=0
+iteration_list=list()#to see the all the numbers that were added
+for i in range(1,101):
+    iteration_list.append(i)
+    total+=i
+print(total,"\n",iteration_list)
 
 
 
--- Theory testing/while-loop.py
+++ Theory testing/while-loop.py
@@ -1,4 +1,14 @@
+#printing hello world 5 times
 x = 1
 while x<6 :
     print ("Hello World")
     x+=1
+
+
+#print all the sum of numbers from 1 to 100 
+x =100
+total =0
+while x!=0:
+    total+=x
+    x -=1
+print(total)
